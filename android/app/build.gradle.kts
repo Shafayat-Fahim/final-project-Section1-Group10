b55e5545c6d1f167bf266134b338afa21f601788
Shafayat Fahim <theanonto@outlook.com>
2025-12-09 03:04:49 +0600

    Updated 'minSdk' to 23 in 'android/app/build.gradle.kts'
    API 23 corresponds to Android 6.0 (Marshmallow), released in 2015.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.final_project"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23     //API 23 corresponds to Android 6.0 (Marshmallow), released in 2015.
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
