b9a0e24541428430dd92b6e63e3140c612da3f8d
Shafayat Fahim <theanonto@outlook.com>
2025-12-09 04:07:49 +0600

    Updated 'android/build.gradle.kts' and 'android/app/build.gradle.kts'
    minSdk = 23
    ndkVersion = "27.0.12077973"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+apply(plugin = "com.google.gms.google-services")
 
 android {
     namespace = "com.example.final_project"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,11 +21,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.final_project"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23     //API 23 corresponds to Android 6.0 (Marshmallow), released in 2015.
+        applicationId = "com.example.final_project" // Must match google-services.json
+        minSdk = 23     // API 23 corresponds to Android 6.0 (Marshmallow), released in 2015.
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
